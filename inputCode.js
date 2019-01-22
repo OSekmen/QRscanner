1281a2f02b1a6dee737e1dc34115b333bf7e8bf4
OSekmen <46937646+OSekmen@users.noreply.github.com>
2019-01-22 19:35:46 +0100

    Add files via upload

---
+++ inputCode.js
@@ -0,0 +1,70 @@
+﻿
+function decode(content) {
+     //scanner enable
+    var dict = {
+        data: [
+            { code: "30", value: "0" },
+            { code: "31", value: "1" },
+            { code: "32", value: "2" },
+            { code: "33", value: "3" },
+            { code: "34", value: "4" },
+            { code: "35", value: "5" },
+            { code: "36", value: "6" },
+            { code: "37", value: "7" },
+            { code: "38", value: "8" },
+            { code: "39", value: "9" },
+            { code: "21", value: "e" },
+            { code: "22", value: "pi"},
+            { code: "2D", value: "E" },
+            { code: "2E", value: "." },
+            { code: "48", value: "x" },
+            { code: "C91A", value: "^" }
+            //{ code: "1B", value: "!END" }
+
+        ]
+    };
+    var scanned = [];
+
+    //var content = "http://wes.casio.com/math/index.php?q=I-235F+U-000C00090252+M-C10000AD00+S-001510100000100E1010B0005F8F+R-0125000000000000010200000000000000000000+E-3548C91A321B";
+    
+    QRinput = content.slice(content.indexOf("E-") + 2, content.length);
+    console.log(QRinput);
+
+    var buffer = "";
+    for (var i = 0; i <= QRinput.length; i++) {
+
+        buffer = buffer + QRinput.substring(i, i + 1);
+        
+
+        for (var j = 0; j < dict.data.length; j++) {
+            if (buffer == dict.data[j].code) {
+                console.log(dict.data[j].value);
+               // functie = functie + dict.data[j].value;
+                scanned.push(dict.data[j].value );
+                buffer = buffer.replace(buffer, "");
+                
+            }
+            
+        }
+       //console.log(buffer);
+      
+    }
+    console.log(scanned);
+
+    //converter without eval
+    
+    var term = "";
+    for (var i = 0; i < scanned.length; i++) {     
+        switch (scanned[i]) {
+            case '^':
+                term += "Math.pow(" + scanned[i - 1] + "," + scanned[i + 1] + ");";
+                functie += term;
+                term = "";
+                break;
+        }
+    }
+    
+    console.log(functie);
+    plot();
+}
+
---
+++ main.js
@@ -0,0 +1,124 @@
+﻿var capture;
+var scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
+var QRinput;
+var functie = "";
+//var content= "";
+
+    
+
+window.onload = function () {
+    
+    scanQR();
+
+    
+       //  console.log(content);
+       // decode();
+     //   plot();
+    
+   
+    
+   
+
+       
+   
+}
+
+function calc(x) {
+
+    return (eval(functie));
+}
+
+
+
+
+function plot() {
+    var canvas = document.getElementById("graph");
+    var ctx = canvas.getContext("2d");
+
+    //draw axis
+    ctx.beginPath();
+    ctx.fillStyle = "#dddddd";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.strokeStyle = "#000000";
+    ctx.lineWidth = "1";
+    ctx.translate(canvas.width / 2, canvas.height / 2);
+    ctx.scale(1, -1);
+    ctx.moveTo(-canvas.width / 2, 0);
+    ctx.lineTo(canvas.width / 2, 0);
+    ctx.moveTo(0, canvas.width / 2);
+    ctx.lineTo(0, -canvas.width / 2);
+   // ctx.closePath();
+    ctx.stroke(); 
+
+    //draw grid
+    
+    var XGridSpacing = 50;
+    var YGridSpacing = 50;
+    ctx.beginPath();
+    ctx.strokeStyle = "#009933";
+    ctx.lineWidth = "1";
+    //horizontal
+    for (var i = XGridSpacing; i < canvas.height; i += XGridSpacing) {
+        ctx.moveTo((-canvas.width /2), i);
+        ctx.lineTo(canvas.width, i);
+        ctx.moveTo((-canvas.width / 2), -i);
+        ctx.lineTo(canvas.width, -i);
+    }
+    //vertical
+    for (var i = YGridSpacing; i < canvas.height; i += YGridSpacing) {     
+        ctx.moveTo(i, (canvas.height / 2));
+        ctx.lineTo(i, -canvas.height);
+        ctx.moveTo(-i, (canvas.height / 2));
+        ctx.lineTo(-i, -canvas.height);
+    }
+    //ctx.closePath();
+    ctx.stroke();
+    
+
+    
+    //Graph and data
+    var data = { X: [], Y: [] }; 
+    //add all x values to array
+    for (var i = -50; i < 50; i+= 0.1) {
+        data.X.push(i);
+    }
+
+   //calculate all Y values & add to array
+    for (var i = 0; i < data.X.length; i++) {
+        data.Y.push(calc(data.X[i]));
+    }
+
+    //draw graph
+    ctx.beginPath();
+    ctx.lineWidth = "2";
+    ctx.strokeStyle = "#2818bc";
+    for (var i = 0; i < data.X.length; i++) {
+        ctx.lineTo(data.X[i] * XGridSpacing, data.Y[i] * YGridSpacing);
+    }
+    ctx.stroke();
+}
+
+function scanQR(contentR) {
+
+    Instascan.Camera.getCameras().then(cameras => {
+        if (cameras.length > 0) {
+            scanner.start(cameras[0]);
+        } else {
+            console.error("Please enable Camera!");
+        }
+    });
+    scanner.addListener('scan', function (content) {
+        alert('Do you want to open this page?: ' + content);
+        console.log(content);
+        //window.open(content, "_blank"); //Open Casio site.
+        scanner.stop();
+        console.log(content);
+        decode(content);
+    });
+
+    
+}
+
+    
+
+
