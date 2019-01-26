1e0573d4761fe6512f90996c982de5f248240a0d
OSekmen <46937646+OSekmen@users.noreply.github.com>
2019-01-26 21:31:21 +0100

    update

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 ﻿var canvas = document.getElementById("graph");
 var ctx = canvas.getContext("2d");
+var Zcanvas = document.getElementById("layer");
+var ctz = Zcanvas.getContext("2d");
 
 window.onload = function () {
 
 
 
     ctx.translate(canvas.width / 2, canvas.height / 2);
-    scanQR(); //use camera
-    //decode(content); //use build in template
+    //scanQR(); //use camera
+    decode(content); //use build in template
     trackMouse();
 }
 
@@ -16,14 +18,18 @@ function calc(x) {
 }
 
 function trackMouse(event) {
-    var xPix = event.pageX - 10;
-    var yPix = event.pageY - 10;
+
+    var xPix = event.pageX;
+    var yPix = event.pageY;
     var xCan = ((xPix / XGridSpacing) - ((canvas.width / 2) / (XGridSpacing)));
     var yCan = -((yPix / YGridSpacing) - ((canvas.height / 2) / (YGridSpacing)));
     console.log(xPix, yPix);
-    ctx.beginPath();
-    ctx.arc(xPix - canvas.width / 2, yPix - canvas.height / 2, 5, 0, 2 * Math.PI)
-    ctx.stroke();
+    ctz.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight)
+
+    ctz.beginPath();
+    ////snap on graph points
+       ctz.arc(xPix, yPix, 5, 0, 2 * Math.PI);
+    ctz.stroke();
     
 }
 
