a6f83a92ab198dfa11e8e9afd5b9f18f1abee0ae
OSekmen <46937646+OSekmen@users.noreply.github.com>
2019-01-27 20:54:35 +0100

    debug

--- inputCode.js
+++ inputCode.js
@@ -125,8 +125,8 @@ function scanQR() {
     function tick() {
 
         if (video.readyState === video.HAVE_ENOUGH_DATA) {
-            //canvasElement.height = video.videoHeight;
-            //canvasElement.width = video.videoWidth;
+            canvasElement.height = video.videoHeight;
+            canvasElement.width = video.videoWidth;
             canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
             var imageData = canvas.getImageData(0, 0, canvasElement.width, canvasElement.height);
             var code = jsQR(imageData.data, imageData.width, imageData.height, {
