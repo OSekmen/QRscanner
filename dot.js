e25dc9c4ec668c301ce12a12484660b30fafb500
OSekmen <46937646+OSekmen@users.noreply.github.com>
2019-01-28 16:03:57 +0100

    CalcGraph V1.00

--- dot.js
+++ dot.js
@@ -1,5 +1,20 @@
-var dot = function () {
+class Dot {
+   
+    constructor() {
+    }
 
+    show(xCan) {
+        var x = reverseXSnap(xCan);
+        var y = reverseYSnap(calc(Math.round(xCan)));
+
+        ctz.beginPath();
+        ctz.arc(x, y, 5, 0, 2 * Math.PI);
+        ctz.stroke();
+        ctz.font = "20px Arial";
+        
+        ctz.fillText("(" + String(Math.round(xCan)) +", " +String(calc(Math.round(xCan))) + ")", x + 50, y - 50);
+        
+    }
 
 }
 
